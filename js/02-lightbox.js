6b8ba5010d0a9e0dca22ccc8410477b208b8f923
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-05-12 14:19:17 +0300

    Виконане дз 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+
+const createItem = ({preview,original,description}) => `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`;
+
+
+
+const createGallery = galleryItems.map(createItem).join("");
+
+gallery.insertAdjacentHTML("beforeend",createGallery);
 console.log(galleryItems);
+
+
+
+
+const lightbox  = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,  
+  });
+console.log(lightbox);
